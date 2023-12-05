8b67b9d200ef97b0ef746c105d4b7a9d66b24967
Roshan Thomas <roshtho@amazon.com>
2023-12-05 22:00:30 +1100

    Updated the Maximo Manage Channel to 8.7.x

--- scripts/add_maximo_manage.sh
+++ scripts/add_maximo_manage.sh
@@ -29,4 +29,4 @@ export MAS_CONFIG_DIR=/root/install-dir/masconfig
 
 echo `date "+%Y/%m/%d %H:%M:%S"` "Deploying Maximo Manage on MAS"
 # Deploy Maximo Manage Application
-export ROLE_NAME=suite_app_install export MAS_APP_ID=manage && export MAS_APP_CHANNEL="8.6.x" && export MAS_WORKSPACE_ID=masdev && ansible-playbook ibm.mas_devops.run_role
+export ROLE_NAME=suite_app_install export MAS_APP_ID=manage && export MAS_APP_CHANNEL="8.7.x" && export MAS_WORKSPACE_ID=masdev && ansible-playbook ibm.mas_devops.run_role
